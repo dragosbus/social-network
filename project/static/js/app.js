d84933c56c81039ac3744f61d57ffd7bca1da7ca
dragosbus <dragosbusuioc94@gmail.com>
2018-06-24 15:50:49 +0200

    alert the user if the password dont match

--- project/static/js/app.js
+++ project/static/js/app.js
@@ -1,6 +1,11 @@
 const App = (function() {
 
-  let model = {username:'', user_email:'', user_pass:'', repeat_pass:''};
+  let model = {
+    username: '',
+    user_email: '',
+    user_password: '',
+    repeat_password: ''
+  };
 
   function init() {
     const form = document.querySelector('form');
@@ -10,39 +15,53 @@ const App = (function() {
 
   function formEventHandler(e) {
     e.preventDefault();
-    if(e.target.classList.contains('register')) registerHandler();
-  }
+    if (e.target.classList.contains('register'))
+      registerHandler();
+    }
 
   function registerHandler() {
-    let username = document.getElementById('username').value;
-    let user_email = document.getElementById('user-email').value;
-    let user_pass = document.getElementById('user-password').value;
-    let repeat_pass = document.getElementById('repeat-password').value;
+    let {username, user_email, user_password, repeat_password} = model;
 
-    model = {username, user_email, user_pass, repeat_pass};
-    console.log(model);
   }
 
   function inputOnChange() {
     const username = document.getElementById('username');
-    const user_email = document.getElementById('user-email');
-    const user_pass = document.getElementById('user-password');
-    const repeat_pass = document.getElementById('repeat-password');
+    const user_email = document.getElementById('user_email');
+    const user_pass = document.getElementById('user_password');
+    const repeat_pass = document.getElementById('repeat_password');
 
     let inputs = [username, user_email, user_pass, repeat_pass];
-    inputs.forEach(input=>{
-      input.addEventListener('change', e=>{
-        if(e.target.id === 'username') model.username = e.target.value;
-        else if(e.target.id === 'user-email') model.user_email = e.target.value;
-        else if (e.target.id === 'user-password') model.user_pass = e.target.value;
-        else if (e.target.id === 'repeat-password') model.repeat_pass = e.target.value;
-
-        let {username, user_email, user_pass, repeat_pass} = model;
-        if(username && user_email && user_pass && repeat_pass) {
-          document.getElementById('register-btn').disabled = false;
+    inputs.forEach(input => {
+      input.addEventListener('input', e => {
+        setModelProp(e.target, 'username');
+        setModelProp(e.target, 'user_email');
+        setModelProp(e.target, 'user_password');
+        setModelProp(e.target, 'repeat_password');
+        setBorderColor(e.target);
+        let {username, user_email, user_password, repeat_password} = model;
+        //check if all inputs are not empty then check if passwords match
+        if (!username || !user_email || !user_password || !repeat_password) {
+          document.getElementById('register-btn').disabled = true;
+        } else {
+          if (user_password !== repeat_password) {
+            document.getElementById('register-btn').disabled = true;
+            document.querySelector('.not-match').classList.remove('hide');
+          } else {
+            document.getElementById('register-btn').disabled = false;
+            document.querySelector('.not-match').classList.add('hide');
+          }
         }
-      });
-    });
+      }); //end change event listener
+    }); //end forEach
+  }
+
+  function setModelProp(target, prop) {
+    if(target.id === prop) model[prop] = target.value
+  }
+
+  function setBorderColor(target) {
+    if(target.value) target.style.border = '2px solid #3ac162';
+    else target.style.border = '2px solid #f21541';
   }
 
   return {init};
