221e315d86102c3c17c8d679079c064f76cab3bb
dragosbus <dragosbusuioc94@gmail.com>
2018-06-24 12:07:53 +0200

    init templates files and static files

--- project/app.py
+++ project/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
-from flask.ext.login import UserMixin
+from flask import Flask, url_for, request, redirect, render_template
 import model
 
 
@@ -9,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
